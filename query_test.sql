435ce10a9fb2dbea060a585af6d62406a7c0cfdf
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2019-06-23 19:59:47 +0700

    delete fille

--- query_test.sql
+++
@@ -1,9 +0,0 @@
-use `nodejsimages`;
-SELECT * FROM users WHERE id = 8;
-SELECT * FROM photos WHERE id = 17;
-UPDATE photos SET title = "thay doi"  WHERE id = 17 and id_user = 4;
-
-SELECT * FROM users WHERE id = 4 and pass = "1"
-SELECT * FROM users WHERE id = 4
-SELECT * FROM photos ORDER BY id DESC LIMIT 10;
-SELECT * FROM photos WHERE id = 2;
